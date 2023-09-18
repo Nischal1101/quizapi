ec8dfc855eab44844de2c75ae425234407f271fd
Nischal <kaflenk123.gmail.com>
2023-09-18 19:23:10 +0545

    added authentication

--- src/middlewares/Authentication.ts
+++ src/middlewares/Authentication.ts
@@ -2,24 +2,23 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { SECRET } from "../config";
 const auth = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const header = req.headers.authorization;
-    if (!header) {
-      return res.json({ msg: "NO authentication header found!" });
-    }
-    const token = header.split(" ")[1];
-    let user;
-    try {
-      user = jwt.verify(token, SECRET!);
-    } catch (error) {
-      next("Invalid token");
-    }
-    if (!user) {
-      return res.json({ msg: "User not authorized." });
-    }
-    next();
-  } catch (error) {
-    next(error + "hi");
+  const header = req.headers.authorization;
+  if (!header) {
+    return res.json({ msg: "NO authentication header found!" });
   }
+  const token = header.split(" ")[1];
+  let decodedtoken:{userId:String};
+  try {
+    decodedtoken = <any>jwt.verify(token, SECRET!);
+    console.log(decodedtoken)
+    // decodedtoken={userId:"kjfdd"};
+  } catch (error) {
+    next("User not authorized.");
+  }
+  // if (!decodedtoken) {
+    // return res.json({ msg: "User not authorized." });
+  // }
+  // req.user = user.userId;
+  next();
 };
 export default auth;
